08d5e6f162bcb62431ccb78beee2e4042a23749c
Preston Thomsen <preston.thomsen@esmsnyc.net>
2020-06-05 16:07:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function placeOrder(cardNumber) {
   if (cardNumber == undefined) {
     return "Sorry, we don't have a credit card on file for you."
   } else {
-    var end = `your total cost is $${total()}, which will be charged to the card ${cardNumber}`
+    var end = `your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
     cart = []
     return end
   }
