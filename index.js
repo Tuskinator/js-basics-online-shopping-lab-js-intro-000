720de1918644015e503024fd17e547afbd93ecc0
Preston Thomsen <preston.thomsen@esmsnyc.net>
2020-06-05 16:05:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -80,5 +80,7 @@ if (item == cart[placement].itemName) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber == undefined) {
+    return "Sorry, we don't have a credit card on file for you."
+  }
 }
